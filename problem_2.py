79bf4e06938b907c3a1d9e082d31b61f9642e363
Alfonso Gonzalez <alfonsog@gmail.com>
2019-06-02 22:00:04 +0100

    Fix function return requirement. Add test cases to handle edge cases. Thank you

--- problem_2.py
+++ problem_2.py
@@ -1,5 +1,11 @@
 import os
 
+def is_list(element):
+    """
+    Check if element is a Python list
+    """
+    return isinstance(element, list)
+
 def find_files(suffix, path):
     """
     Find all files beneath path with file name suffix.
@@ -16,12 +22,66 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    for file in sorted(os.listdir(path)):
-        if os.path.isfile(os.path.join(path,file)):
-            if file.endswith(suffix):
-                print(os.path.join(path,file))
-        elif os.path.isdir(os.path.join(path,file)):
-            find_files(suffix, os.path.join(path,file))
 
+    new_files = []
 
-find_files(".c", "./testdir")
+    if os.path.isfile(path):
+        if path.endswith(suffix):
+            # print("Returning.. ", path)
+            return path
+    elif os.path.isdir(path):
+        # print(os.listdir(path))
+        for entry in sorted(os.listdir(path)):
+            output = find_files(suffix,os.path.join(path, entry))
+            # print("Output ", output)
+            if is_list(output):
+                new_files = new_files + output
+            elif output is not None:
+                new_files.append(output)
+        if new_files != []:
+            return new_files
+        else:
+            return None
+    else:
+        return None
+
+def test_function(test_case):
+    suffix = test_case[0]
+    path = test_case[1]
+    solution = test_case[2]
+
+    print("Find files with extension %s in the %s directory" % (suffix, path))
+    output = find_files(suffix, path)
+    # print(output)
+    if output == solution:
+        print("Pass")
+    else:
+        print("False")
+
+# Find all files with .c extension in ./testdir
+path = './testdir'
+suffix = '.c'
+solution = ['./testdir/subdir1/a.c', './testdir/subdir3/subsubdir1/b.c', './testdir/subdir5/a.c', './testdir/t1.c']
+test_case = [suffix, path, solution]
+test_function(test_case)
+
+# Find all files with .py extension in ./testdir
+path = './testdir'
+suffix = '.py'
+solution = None
+test_case = [suffix, path, solution]
+test_function(test_case)
+
+# Find all files with empty extension in ./testdir
+path = './testdir4'
+suffix = ''
+solution = None
+test_case = [suffix, path, solution]
+test_function(test_case)
+
+# Find all files with .c extension in ./testdir4 which doesn't exist
+path = './testdir4'
+suffix = '.c'
+solution = None
+test_case = [suffix, path, solution]
+test_function(test_case)
